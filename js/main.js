b456d83cbf05047c2bd78a368bcefbc885d79dbc
James Blackman <JBlack262@west-mec.org>
2021-01-29 16:00:43 -0700

    added content and store button

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function storePage() {
+    var x = Math.floor(Math.random() * 5);
+    if (x == 0) {
+        window.location.href = "civic.html";
+    } else if (x == 1) {
+        window.location.href = "bugatti.html";
+    } else if (x == 2) {
+        window.location.href = "lambo.html";
+    } else if (x == 3) {
+        window.location.href = "mustang.html";
+    } else if (x == 4) {
+        window.location.href = "tesla.html";
+    }
+}
